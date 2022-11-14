982851ef4523aab59f2fe4bb80a4b9ac03c3e7a9
krystalkk <krystalhuangkk@gmail.com>
2022-11-14 14:55:47 -0500

    2

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict'
+const meow = require('meow')
+const tangPoems = require('.')
+
+const cli = meow(`
+  Usage
+    $ poem [-a]
+  Examples
+    $ poem 
+    $ poem -a
+`);
+
+let isALL = false
+
+if (cli.flags.a || cli.flags.all) {
+  isALL = true
+}
+
+(async () => {
+  const poem = await tangPoems(isALL)
+  console.log(poem)
+})();
+
--- index.js
+++ index.js
@@ -2,19 +2,57 @@ console.log("hi!")
 
 // Import the express library and assign it to a variable
 import express from 'express'
-
+import fetch from 'node-fetch'
 // Create an instance of an express application 
 const app = express()
+app.use(express.json())
 
 // Set the port the application will be running on
 const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
 app.get('/', (req, res) => {
-  res.send("draw a finger")
+  console.log("test")
+  res.json({ data: "response goes here" })
 })
+
+app.get('/sleep/:time', (req, res) => {
+  console.log(req.params)
+  res.json({ data: "sleeptime" })
+})
+
 
 // Set the application to listen a port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+// Example of an application route that makes a request to another server
+app.get('/advice', async (req, res) => {
+  // Make a request to another wbesite and wait for a response
+  const response = await fetch('https://api.adviceslip.com/advice')
+
+  // Read the response
+  const body = await response.json()
+
+  // Print the repsonse body to the console
+  console.log(body)
+
+  // Get the advice text string from the response body object
+  const advice = body.slip.advice
+
+  res.json({ data: advice })
+})
+
+
+
+const got = require('got')
+
+module.exports = async (isAll) => {
+  const response = await got('https://v2.jinrishici.com/one.json')
+  const body = JSON.parse(response.body)
+  if (isAll) {
+    return body.data.origin.content
+  } else {
+    return body.data.content
+  }
+}
